8ca924a27c17ccb8f58729e217d66541678ea645
maulana-99 <139872583+maulana-99@users.noreply.github.com>
2024-05-17 10:42:05 +0700

    register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Redirect;
 
-class SesiController extends Controller
+class AuthController extends Controller
 {
     public function index()
     {
@@ -50,5 +52,32 @@ class SesiController extends Controller
     {
         Auth::logout();
         return redirect('/home');
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate(
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ],
+            [
+                'name.required' => 'Username wajib di isi',
+                'email.required' => 'Email wajib di isi',
+                'email.unique' => 'Email sudah terdaftar',
+                'password.required' => 'Password wajib di isi',
+                'password.min' => 'Password minimal 8 karakter',
+                'password.confirmed' => 'Konfirmasi password tidak cocok',
+            ]
+        );
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json(['message' => 'User successfully registered', 'user' => $user], 201);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\SesiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 // menjadi mode tamu dan hanya bisa melihat saja
 Route::middleware(['guest'])->group(function () {
 
-    Route::get('/', [SesiController::class, 'index'])->name('login');
-    Route::post('/', [SesiController::class, 'login']);
+    Route::get('/', [AuthController::class, 'index'])->name('login');
+    Route::post('/', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
 });
 
@@ -31,7 +32,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->middleware('userAkses:admin');
     Route::get('/resepsionis', [AdminController::class, 'resepsionis'])->middleware('userAkses:resepsionis');
     Route::get('/tamu', [AdminController::class, 'tamu'])->middleware('userAkses:tamu');
-    Route::get('/logout', [SesiController::class, 'logout']);
+    Route::get('/logout', [AuthController::class, 'logout']);
 
 });
-
