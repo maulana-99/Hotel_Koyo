c7fe331976ae4cb7026b53347010cb5e977f5aea
rafidalifsyah <169132746+rafidalifsyah@users.noreply.github.com>
2024-05-21 09:57:41 +0700

    ganti nama

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="{{asset('css/login_tamu.css')}}">
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
     <title>Login Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
