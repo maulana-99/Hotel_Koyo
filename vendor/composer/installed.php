692983bd7d35a2ea08d0a3266cf554316a5fe8a5
rafidalifsyah <169132746+rafidalifsyah@users.noreply.github.com>
2024-05-26 13:24:40 +0700

    test

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '743ea7616f458eb90f5e374ed99b7e6d2bac3e00',
+        'reference' => '047ad1e2c6c78498d9164751e487f8e79f27046b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '743ea7616f458eb90f5e374ed99b7e6d2bac3e00',
+            'reference' => '047ad1e2c6c78498d9164751e487f8e79f27046b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
